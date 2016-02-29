3835f085feba631f0aa2cd32013a88fb004b7ffa
Berkus Decker <berkus@madfire.net>
2016-02-29 05:54:22 +0200

    Depend on filesystem always, since it's used for logging in arsenal now

--- setup.cmake
+++ setup.cmake
@@ -129,10 +129,8 @@ list(APPEND BOOST_COMPONENTS date_time)
 list(APPEND BOOST_COMPONENTS program_options)
 # Thread library used in some nat libs and tests
 list(APPEND BOOST_COMPONENTS thread)
-if (UNIX AND NOT APPLE)
-    # Filesystem library used on Linux for creating config directories
-    list(APPEND BOOST_COMPONENTS filesystem)
-endif (UNIX AND NOT APPLE)
+# Filesystem library used on Linux for creating config directories
+list(APPEND BOOST_COMPONENTS filesystem)
 
 set(Boost_USE_STATIC_LIBS ON)
 find_package(Boost REQUIRED COMPONENTS ${BOOST_COMPONENTS})
